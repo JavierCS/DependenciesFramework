40632264bebedc441af040d4c78dfb9c07ffaee7
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-13 01:29:20 -0600

    [NEW] - Se agrega DFController en la carpeta TestGroup para probar comunicación y visibilidad del framework a la app principal

---
+++ DependenciesFramework/TestGroup/DFController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+public class DFController: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+}
