5c643080ec912577fc4bc33e541deb17fec0a5a8
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-12 22:17:17 -0600

    Initial Commit

---
+++ DependenciesFramework/DependenciesFramework.h
@@ -0,0 +1,18 @@
+//
+//  DependenciesFramework.h
+//  DependenciesFramework
+//
+//  Created by Javier Cruz Santiago on 12/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependenciesFramework.
+FOUNDATION_EXPORT double DependenciesFrameworkVersionNumber;
+
+//! Project version string for DependenciesFramework.
+FOUNDATION_EXPORT const unsigned char DependenciesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependenciesFramework/PublicHeader.h>
+
+
